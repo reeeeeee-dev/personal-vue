99bbd8ae849cfaff59315bd4e153d1fb2907918c
Reetik Patel <reetikp@gmail.com>
2024-06-21 22:00:29 -0500

    👷 CI take 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: process.env.NODE_ENV === 'production' ? '/personal-vue' : '/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
