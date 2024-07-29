2cd575ec9f8c2bb71077974784b7f185fcacb7b3
Reetik Patel <reetikp@gmail.com>
2024-07-29 18:58:15 -0500

    🐛 removed rotation

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="rotate-[-45deg] overflow-hidden md:min-xl:h-screen flex flex-wrap m-16 mt-96 items-center justify-center"
+    class="overflow-hidden md:min-xl:h-screen flex flex-wrap m-16 mt-96 items-center justify-center"
   >
     <div class="rotate-90 m-[-128px]">
       <h1 class="text-[80px] bg-black p-4">Reetik Patel</h1>
     </div>
     <div class="h-[50vh] w-[50vh] rounded-3xl overflow-hidden">
       <img
-        class="rotate-45 object-cover scale-[1.45] mt-[-450px] ml-12 sepia-[0.3]"
+        class="object-cover scale-[1.45] mt-[-450px] ml-12 sepia-[0.3]"
         src="/img/headshot.jpg"
       />
     </div>
