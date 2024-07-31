9269e055a276c6df87be56900221b6da048c520f
Reetik Patel <reetikp@gmail.com>
2024-07-31 00:05:36 -0500

    🐛 fixed bright block on home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import HeroSection from '@/components/HeroSection.vue'
 </script>
 
 <template>
-  <main class="bg-[var(--dark)]">
+  <main class="h-screen bg-[var(--dark)]">
     <div>&nbsp;</div>
     <HeroSection />
     <div>&nbsp;</div>
