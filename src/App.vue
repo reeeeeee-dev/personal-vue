ec419588ee89fd35bd0deb330c31045e98a92c62
Reetik Patel <reetikp@gmail.com>
2024-07-31 00:03:32 -0500

    🏗️ moved journey to separate page

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper"></div>
+  <header class="bg-[var(--dark)] flex justify-end gap-8 px-16 py-8">
+    <RouterLink to="/" class="hover:text-[var(--accent)] transition-colors">Home</RouterLink>
+    <RouterLink to="/journey" class="hover:text-[var(--accent)] transition-colors">
+      Journey
+    </RouterLink>
+    <RouterLink to="/projects" class="hover:text-[var(--accent)] transition-colors">
+      Projects
+    </RouterLink>
   </header>
 
   <RouterView />
--- src/views/JourneyView.vue
+++ src/views/JourneyView.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HeroSection from '@/components/HeroSection.vue'
+import HistorySection from '@/components/HistorySection.vue'
 </script>
 
 <template>
   <main class="bg-[var(--dark)]">
     <div>&nbsp;</div>
-    <HeroSection />
+    <HistorySection />
     <div>&nbsp;</div>
   </main>
 </template>
