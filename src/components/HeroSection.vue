42a50f44ab3c092e4841e3d05dd764a6e8014593
Reetik Patel <reetikp@gmail.com>
2024-08-09 00:25:06 -0500

    🐛 fixed hero centering

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,7 +3,9 @@ import Parallaxy from '@lucien144/vue3-parallaxy'
 </script>
 
 <template>
-  <div class="bg-cover bg-no-repeat bg-[url(/img/shades.jpg)] flex h-96 px-32 py-64 items-center">
+  <div
+    class="bg-cover bg-no-repeat bg-[url(/img/shades.jpg)] flex h-96 px-32 py-64 items-center justify-center"
+  >
     <div class="text-9xl text-[var(--dark)] p-16">Reetik Patel</div>
     <img src="/img/headshot.jpg" class="h-96" />
   </div>
