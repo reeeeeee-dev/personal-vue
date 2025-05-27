68988b19eb11d2389982fe8b0ea4475ecdd0c608
Reetik Patel <reetikp@gmail.com>
2025-05-27 18:07:24 -0500

    🚧 house

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -13,6 +13,12 @@ const scrollToElem = (id) => document.getElementById(id).scrollIntoView({ behavi
         >
           Resplash
         </li>
+        <li
+          class="pl-8 hover:text-[var(--accent)] transition-colors cursor-pointer"
+          @click="scrollToElem('43south')"
+        >
+          43 South Smart Home & Servers
+        </li>
       </ul>
     </div>
 
@@ -22,7 +28,7 @@ const scrollToElem = (id) => document.getElementById(id).scrollIntoView({ behavi
       <div>
         <div class="w-min">
           <h3 class="text-6xl w-max">Resplash</h3>
-          <div stop>Native Android</div>
+          <div>Native Android</div>
         </div>
         <p class="max-w-prose mt-4">
           This was one of my first forays in native Android development. Resplash is always one of
@@ -48,6 +54,34 @@ const scrollToElem = (id) => document.getElementById(id).scrollIntoView({ behavi
         src="https://raw.githubusercontent.com/b-lam/Resplash/master/design/Resplash_Play_Store_Banner.jpg"
         class="h-64 bg-white"
       />
+    </div>
+
+    <br id="43south" class="my-32" />
+
+    <div class="flex flex-row-reverse gap-32 h-full mt-8 flex-wrap">
+      <div>
+        <div class="w-min">
+          <h3 class="text-6xl w-max">43 South</h3>
+          <marquee>
+            Small Electronics | Home Assistant | Shelly | Unraid | Server Management | IoT |
+            Ubiquiti Unifi
+          </marquee>
+        </div>
+        <p class="max-w-prose mt-4">
+          43 South is my parent's new house. When moving into this house, I wasn't swayed much by
+          the size, neighborhood, or anything. The one thing that caught me was the wiring. Built in
+          2006, the house contained coaxial, Cat5e ethernet, and alarm wiring. I started in the
+          garage setting up my servers. The network and security camera's ran on Ubiquiti's Unifi
+          line of products. The main server was an old Dell C2100 I bought off a friend for $100.
+          Over time I upgraded the RAM, CPUs, and added a total of 50TB of storage. The server ran
+          everything from a simple DDNS client, game servers, to multiple virtual machines alongside
+          archival storage for my family. Throughout the house, I installed Unifi access points and
+          cameras, Shelly smart relays, and other sensors to automate different things. Among them
+          were the garage door, syncing outdoor lighting to the solar cycle, and improving network
+          coverage.
+        </p>
+      </div>
+      <img src="/img/43south.jpg" class="h-80 bg-white" />
     </div>
   </div>
 </template>
