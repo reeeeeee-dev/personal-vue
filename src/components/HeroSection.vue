dceb4ea808e210f24a2577eac17db24522f1153a
Reetik Patel <reetikp@gmail.com>
2025-10-26 16:40:33 -0500

    💄🐛  fixed small screen

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,13 +1,11 @@
-<script setup>
-import Parallaxy from '@lucien144/vue3-parallaxy'
-</script>
+<script setup></script>
 
 <template>
   <div
-    class="bg-cover bg-no-repeat bg-[url(/img/shades.jpg)] flex h-96 px-32 py-64 items-center justify-center"
+    class="bg-cover bg-no-repeat bg-[url(/img/shades.jpg)] flex px-32 py-64 items-center justify-center flex-wrap"
   >
     <div class="text-9xl text-[var(--dark)] p-16">Reetik Patel</div>
-    <img src="/img/headshot.jpg" class="h-96" />
+    <img src="/img/headshot.jpg" class="h-96 w-96 aspect-square object-contain" />
   </div>
   <div class="flex flex-wrap justify-evenly">
     <div class="p-8 flex gap-8">
